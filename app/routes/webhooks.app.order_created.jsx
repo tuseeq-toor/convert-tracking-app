9e4f86ac7a7845129ccc7390ba0041022cb7fec7
Tuseeq Toor <tuseeqtoor@yahoo.com>
2025-01-11 02:34:14 +0500

    queued webhooks

--- app/routes/webhooks.app.order_created.jsx
+++ app/routes/webhooks.app.order_created.jsx
@@ -1,158 +1,175 @@
 import { authenticate } from "../shopify.server";
 
 export const action = async ({ request }) => {
-  const { shop, topic, payload } = await authenticate.webhook(request);
+  try {
+    const { shop, topic, payload } = await authenticate.webhook(request);
 
-  console.log(`Received ${topic} webhook for ${shop}`);
+    // Immediately acknowledge the webhook
+    setTimeout(async () => {
+      // Asynchronous processing
+      await processWebhook(shop, topic, payload);
+    }, 0);
 
-  console.log("Paylod: ", payload);
-
-  // Webhook requests can trigger multiple times and after an app has already been uninstalled.
-  // If this webhook already ran, the session may have been deleted previously.
-
-  const revenueGoalId = "100478811";
-
-  // Event subscriptions
-  const convertAttributesStr = payload.note_attributes.find(
-    (attr) => attr.name === "_conv_attrs",
-  )?.value;
-
-  if (convertAttributesStr) {
-    console.log("Convert attributes string retrieved:", convertAttributesStr);
-    await postTransaction(convertAttributesStr, payload);
-  } else {
-    console.log("Convert Attributes retrieval failed.");
+    return new Response("OK", { status: 200 });
+  } catch (error) {
+    console.error("Error processing webhook:", error);
+    return new Response("Internal Server Error", { status: 500 });
   }
+  async function processWebhook(shop, topic, payload) {
+    console.log(`Received ${topic} webhook for ${shop}`);
 
-  // Function to post transaction details
-  async function postTransaction(convertAttributesStr, orderData) {
-    const convertAttributes = JSON.parse(convertAttributesStr);
-    const conversionRate = parseFloat(convertAttributes.conversionRate) || 1; // Default to 1 if not provided
-    console.log(
-      "Conversion attributes parsed successfully:",
-      convertAttributes,
-    );
-    console.log("Conversion rate used:", conversionRate);
-    let totalRevenue = orderData.total_price;
-    let totalProducts = 0;
+    console.log("Paylod: ", payload);
 
-    // Iterate over each product in the checkout event
-    orderData.line_items.forEach((product) => {
-      console.log("Processing product:", product);
-      console.log("Line Item variant id" + product.variant_id);
-      totalProducts += product.quantity;
-    });
+    // Webhook requests can trigger multiple times and after an app has already been uninstalled.
+    // If this webhook already ran, the session may have been deleted previously.
 
-    console.log(
-      "Total products:",
-      totalProducts,
-      "Total revenue:",
-      totalRevenue,
-    );
+    const revenueGoalId = "100478811";
 
-    // Build POST data for subscription products
-    if (totalProducts > 0) {
-      await postConversion(convertAttributes, revenueGoalId);
-      await sendTrackingBeacon(
+    // Event subscriptions
+    const convertAttributesStr = payload.note_attributes.find(
+      (attr) => attr.name === "_conv_attrs",
+    )?.value;
+
+    if (convertAttributesStr) {
+      console.log("Convert attributes string retrieved:", convertAttributesStr);
+      await postTransaction(convertAttributesStr, payload);
+    } else {
+      console.log("Convert Attributes retrieval failed.");
+    }
+
+    // Function to post transaction details
+    async function postTransaction(convertAttributesStr, orderData) {
+      const convertAttributes = JSON.parse(convertAttributesStr);
+      const conversionRate = parseFloat(convertAttributes.conversionRate) || 1; // Default to 1 if not provided
+      console.log(
+        "Conversion attributes parsed successfully:",
         convertAttributes,
-        totalRevenue,
+      );
+      console.log("Conversion rate used:", conversionRate);
+      let totalRevenue = orderData.total_price;
+      let totalProducts = 0;
+
+      // Iterate over each product in the checkout event
+      orderData.line_items.forEach((product) => {
+        console.log("Processing product:", product);
+        console.log("Line Item variant id" + product.variant_id);
+        totalProducts += product.quantity;
+      });
+
+      console.log(
+        "Total products:",
         totalProducts,
-        revenueGoalId,
+        "Total revenue:",
+        totalRevenue,
       );
-    }
-  }
 
-  //function to post conversion
-  async function postConversion(convertAttributes, goalId) {
-    console.log(
-      "Convert: Triggering Conversion Shopify Customer Event with goal id:",
+      // Build POST data for subscription products
+      if (totalProducts > 0) {
+        await postConversion(convertAttributes, revenueGoalId);
+        await sendTrackingBeacon(
+          convertAttributes,
+          totalRevenue,
+          totalProducts,
+          revenueGoalId,
+        );
+      }
+    }
+
+    //function to post conversion
+    async function postConversion(convertAttributes, goalId) {
+      console.log(
+        "Convert: Triggering Conversion Shopify Customer Event with goal id:",
+        goalId,
+      );
+      const post = {
+        cid: convertAttributes.cid,
+        pid: convertAttributes.pid,
+        seg: convertAttributes.defaultSegments,
+        s: "shopify",
+        vid: convertAttributes.vid,
+        ev: [
+          {
+            evt: "hitGoal",
+            goals: [goalId],
+            exps: convertAttributes.exps,
+            vars: convertAttributes.vars,
+          },
+        ],
+      };
+      const data = JSON.stringify(post);
+      console.log("Data for Conversion:", data);
+      try {
+        const res = await fetch(
+          `https://${convertAttributes.pid}.metrics.convertexperiments.com/track`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json", // Ensure the content type is correct
+            },
+            body: data,
+          },
+        );
+        console.log("Conversion Data Successfully Sent", {
+          status: res?.ok,
+          message: res?.statusText,
+          orderNumber: payload?.order_number,
+        });
+      } catch (error) {
+        console.log("Error Sending Conversion Data", {
+          error,
+          orderNumber: payload?.order_number,
+        });
+      }
+    }
+
+    // Function to send tracking data using Beacon API
+    async function sendTrackingBeacon(
+      attributes,
+      revenue,
+      productCount,
       goalId,
-    );
-    const post = {
-      cid: convertAttributes.cid,
-      pid: convertAttributes.pid,
-      seg: convertAttributes.defaultSegments,
-      s: "shopify",
-      vid: convertAttributes.vid,
-      ev: [
-        {
-          evt: "hitGoal",
-          goals: [goalId],
-          exps: convertAttributes.exps,
-          vars: convertAttributes.vars,
-        },
-      ],
-    };
-    const data = JSON.stringify(post);
-    console.log("Data for Conversion:", data);
-    try {
-      const res = await fetch(
-        `https://${convertAttributes.pid}.metrics.convertexperiments.com/track`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json", // Ensure the content type is correct
+    ) {
+      const post = {
+        cid: attributes.cid,
+        pid: attributes.pid,
+        seg: attributes.defaultSegments,
+        s: "shopify",
+        vid: attributes.vid,
+        ev: [
+          {
+            evt: "tr",
+            exps: attributes.exps,
+            vars: attributes.vars,
+            r: revenue, // Revenue already adjusted in the postTransaction function
+            prc: productCount,
+            goals: [goalId],
           },
-          body: data,
-        },
-      );
-      console.log("Conversion Data Successfully Sent", {
-        status: res?.ok,
-        message: res?.statusText,
-        orderNumber: payload?.order_number,
-      });
-    } catch (error) {
-      console.log("Error Sending Conversion Data", {
-        error,
-        orderNumber: payload?.order_number,
-      });
+        ],
+      };
+      const data = JSON.stringify(post);
+      console.log("Data to for Transaction:", data);
+      try {
+        const res = await fetch(
+          `https://${attributes.pid}.metrics.convertexperiments.com/track`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json", // Ensure the content type is correct
+            },
+            body: data,
+          },
+        );
+        console.log("Transactional Data Successfully Sent", {
+          status: res?.ok,
+          message: res?.statusText,
+          orderNumber: payload?.order_number,
+        });
+      } catch (error) {
+        console.log("Error Sending Transactional Data", {
+          error,
+          orderNumber: payload?.order_number,
+        });
+      }
     }
   }
-
-  // Function to send tracking data using Beacon API
-  async function sendTrackingBeacon(attributes, revenue, productCount, goalId) {
-    const post = {
-      cid: attributes.cid,
-      pid: attributes.pid,
-      seg: attributes.defaultSegments,
-      s: "shopify",
-      vid: attributes.vid,
-      ev: [
-        {
-          evt: "tr",
-          exps: attributes.exps,
-          vars: attributes.vars,
-          r: revenue, // Revenue already adjusted in the postTransaction function
-          prc: productCount,
-          goals: [goalId],
-        },
-      ],
-    };
-    const data = JSON.stringify(post);
-    console.log("Data to for Transaction:", data);
-    try {
-      const res = await fetch(
-        `https://${attributes.pid}.metrics.convertexperiments.com/track`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json", // Ensure the content type is correct
-          },
-          body: data,
-        },
-      );
-      console.log("Transactional Data Successfully Sent", {
-        status: res?.ok,
-        message: res?.statusText,
-        orderNumber: payload?.order_number,
-      });
-    } catch (error) {
-      console.log("Error Sending Transactional Data", {
-        error,
-        orderNumber: payload?.order_number,
-      });
-    }
-  }
-
-  return new Response();
 };
